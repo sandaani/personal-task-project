0a464608c18955a119e8c318d4e8018389937f1d
sandaani <sheekhsandaani@gmail.com>
2024-11-06 18:30:19 +0300

    update taskroutes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,5 +3,16 @@ const taskRoutes=(req, res) => {
         getTasks(req, res);
     } else if(req.method === 'POST'){
         createTask(req , res)
-    } else if(req.method === 'PATCH'){}
+    } else if(req.method === 'PATCH'){
+        updateTASK(req, res)
+    } else if (req.method === 'DELETE'){
+        deleteTask(req , res)
+    } else {
+        res.writeHead(404, 'data Not Found', { 'content-type': 'aplication/json'})
+        res.end(JSON.stringify({
+            message: "Unknown Method required."
+        }))
+    }
 }
+
+module.exports=taskRoutes;
