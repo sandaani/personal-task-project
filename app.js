7c5e33f08c52ef106e6a850aa41efaee0079c9b9
sandaani <sheekhsandaani@gmail.com>
2024-11-06 14:58:42 +0300

    commit 3

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const http = require('http');
 const HOSTNAME='localhost'
 const PORT=9000
 
-const server=http.createServer((req) => {});
+const server=http.createServer((req, res) => {
+    if (req.url.startsWith('/tasks')) {
+        taskRoutes(req, res)
+    } else {
+        res.writeHead(404, 'Not Found', {'content-type': 'aplication/json'})
+        res.end(Json.stringify( {
+            message: 'sorry, page not found'
+        }))
+    }
+});
 
 server.listen(PORT, HOSTNAME, () => {
     console.log('server running on port ${PORT}')
